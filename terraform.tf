5792dadf5e94d8dda7f4a0a466b6460a0bb422ea
Eric Reeves <eric@alluvium.com>
2024-02-04 22:33:13 -0600

    Use Vault-Backed Credentials

--- terraform.tf
+++ terraform.tf
@@ -24,4 +24,17 @@ provider "vault" {}
 
 provider "azurerm" {
   features {}
+  client_id       = data.vault_azure_access_credentials.creds.client_id
+  client_secret   = data.vault_azure_access_credentials.creds.client_secret
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+}
+
+data "vault_azure_access_credentials" "creds" {
+  role                        = "tfc"
+  backend                     = "azure"
+  validate_creds              = true
+  num_sequential_successes    = 8
+  num_seconds_between_tests   = 1
+  max_cred_validation_seconds = 300
 }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,13 @@ variable "resource_group_name" {
   default     = "azure-secrets-rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "The Azure Subscription ID"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "The Azure Tenant ID"
+}
