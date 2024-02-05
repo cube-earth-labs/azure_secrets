c694de86346719038334a418b875c0780ab9ae0e
Eric Reeves <eric@alluvium.com>
2024-02-05 09:44:57 -0600

    Drop comments

--- main.tf
+++ main.tf
@@ -37,9 +37,7 @@ resource "azurerm_key_vault_access_policy" "me" {
 resource "azurerm_key_vault_access_policy" "vault" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = "16a4bfe1-aa7a-48bc-9b4d-2e03f2644e7a"
-  # object_id      = "d4ea7622-b421-43ff-9569-c6e5f07c06b9"
-  object_id = "f7b7dc20-f9d7-455f-8d33-da4b9e3bdfd0"
-  # application_id = "faccd918-cd77-4e76-8e02-87d91a4e9139"
+  object_id    = "f7b7dc20-f9d7-455f-8d33-da4b9e3bdfd0"
 
   key_permissions = [
     "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
