ed401bb96a76de803df7e9e2f973ce228a8d8dac
Eric Reeves <eric@alluvium.com>
2024-02-05 14:10:36 -0600

    Update .gitignore

--- scripts/sync.ps1
+++
@@ -1,7 +0,0 @@
-. .\env.ps1
-
-vault write sys/sync/destinations/azure-kv/my-azure-1 `
-    key_vault_uri="$KEY_VAULT_URI" `
-    client_id="$CLIENT_ID" `
-    client_secret="$CLIENT_SECRET" `
-    tenant_id="$TENANT_ID"
