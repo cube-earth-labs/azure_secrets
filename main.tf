d6d0f6f7968bf9f091edec3d84e63ac70436a3f2
Eric Reeves <eric@alluvium.com>
2024-02-05 09:05:34 -0600

    Drop AKV

--- main.tf
+++ main.tf
@@ -4,55 +4,55 @@ data "azurerm_resource_group" "this" {
   name = "azure_secrets"
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                        = "cube-earth-labs-akv"
-  location                    = data.azurerm_resource_group.this.location
-  resource_group_name         = data.azurerm_resource_group.this.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+# resource "azurerm_key_vault" "kv" {
+#   name                        = "cube-earth-labs-akv"
+#   location                    = data.azurerm_resource_group.this.location
+#   resource_group_name         = data.azurerm_resource_group.this.name
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = false
 
-  sku_name = "standard"
-}
+#   sku_name = "standard"
+# }
 
-resource "azurerm_key_vault_access_policy" "me" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+# resource "azurerm_key_vault_access_policy" "me" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_client_config.current.object_id
 
-  key_permissions = [
-    "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
-  ]
+#   key_permissions = [
+#     "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
+#   ]
 
-  secret_permissions = [
-    "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
-  ]
+#   secret_permissions = [
+#     "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
+#   ]
 
-  storage_permissions = [
-    "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "Update", "SetSAS"
-  ]
-}
+#   storage_permissions = [
+#     "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "Update", "SetSAS"
+#   ]
+# }
 
-resource "azurerm_key_vault_access_policy" "vault" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = "16a4bfe1-aa7a-48bc-9b4d-2e03f2644e7a"
-  object_id    = "d4ea7622-b421-43ff-9569-c6e5f07c06b9"
+# resource "azurerm_key_vault_access_policy" "vault" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id    = "16a4bfe1-aa7a-48bc-9b4d-2e03f2644e7a"
+#   object_id    = "d4ea7622-b421-43ff-9569-c6e5f07c06b9"
 
-  key_permissions = [
-    "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
-  ]
+#   key_permissions = [
+#     "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
+#   ]
 
-  secret_permissions = [
-    "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
-  ]
+#   secret_permissions = [
+#     "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
+#   ]
 
-  storage_permissions = [
-    "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "Update", "SetSAS"
-  ]
-}
+#   storage_permissions = [
+#     "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "Update", "SetSAS"
+#   ]
+# }
 
 
-output "akv_uri" {
-  value = azurerm_key_vault.kv.vault_uri
-}
+# output "akv_uri" {
+#   value = azurerm_key_vault.kv.vault_uri
+# }
