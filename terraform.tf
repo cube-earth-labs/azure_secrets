c58500ae2eba16ef5a3f44c93135cb6f6f97b84e
Eric Reeves <eric@alluvium.com>
2024-02-05 02:14:03 -0600

    Cleanup

--- terraform.tf
+++ terraform.tf
@@ -28,18 +28,4 @@ provider "azurerm" {
   client_secret   = var.arm_client_secret
   subscription_id = var.arm_subscription_id
   tenant_id       = var.arm_tenant_id
-  # client_id       = data.vault_azure_access_credentials.creds.client_id
-  # client_secret   = data.vault_azure_access_credentials.creds.client_secret
-  # subscription_id = var.AZURE_SUBSCRIPTION_ID
-  # tenant_id       = var.AZURE_TENANT_ID
-
-}
-
-data "vault_azure_access_credentials" "creds" {
-  role                        = "tfc"
-  backend                     = "azure"
-  validate_creds              = true
-  num_sequential_successes    = 20
-  num_seconds_between_tests   = 2
-  max_cred_validation_seconds = 1200
 }
